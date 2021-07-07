a6503ee8d3405a23866b66258cf4ee860764069c
ahamedsahib <adsahib39@gmail.com>
2021-07-07 23:13:40 +0530

    [Ahamed] Welcome to deck of card program

---
+++ DeckOfCards/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DeckOfCards
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Deck Of Cards Program");
+        }
+    }
+}
